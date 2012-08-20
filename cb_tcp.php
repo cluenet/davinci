8d9f70804f270d96205bbe14dbd879b9113d1464
Mantas Mikulėnas <grawity@gmail.com>
2012-08-20 19:06:16 +0300

    fix

--- cb_tcp.php
+++ cb_tcp.php
@@ -2,22 +2,28 @@
 <?php
 
 chdir(__DIR__);
+require("cb_config.php");
 require("cb_functions.php");
+
+function handle_req($req, $out) {
+	$args = explode(" ", $req);
+	$cmd = array_shift($args);
+
+	switch ($cmd) {
+	case "points":
+		foreach ($args as $nick) {
+			$points = user_get_points($nick);
+			fwrite($out, "$nick:$points\n");
+		}
+		fwrite($out, "OK\n");
+		break;
+	default:
+		fwrite($out, "lolz\n");
+	}
+}
 
 $users = get_db();
 
 $req = rtrim(fgets(STDIN, 512));
-$args = explode(" ", $req);
-$cmd = array_shift($args);
 
-switch ($cmd) {
-case "points":
-	foreach ($args as $nick) {
-		$points = user_get_points($nick);
-		print "$nick $points\n";
-	}
-	print "OK\n";
-	break;
-default:
-	print "lolz\n";
-}
+handle_req($req, STDOUT);
